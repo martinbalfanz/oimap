ddd2d530cae529e254d2c15c81d24d458a26231d
Martin Balfanz <me@martinbalfanz.com>
2014-12-01 00:16:22 +0100

    Initial Commit

---
+++ oimap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  oimap
+//
+//  Created by Martin Balfanz on 01/12/14.
+//  Copyright (c) 2014 Martin Balfanz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ oimap/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  oimap
+//
+//  Created by Martin Balfanz on 01/12/14.
+//  Copyright (c) 2014 Martin Balfanz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
