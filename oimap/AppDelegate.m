0e65b099837243f3aebc91e2dfc804a8c6a288fc
Martin Balfanz <me@martinbalfanz.com>
2014-12-01 00:30:22 +0100

    Init commit.

--- oimap/AppDelegate.m
+++ oimap/AppDelegate.m
@@ -10,17 +10,38 @@
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSMenu *mainMenu;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    [self setupStatusItem];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (void)setupStatusItem
+{
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+
+    [self.statusItem setTitle:@"oimap"];
+//    [self.statusItem setAlternateImage:[NSImage imageNamed:@"name"]];
+    [self.statusItem setHighlightMode:YES];
+    [self.statusItem setMenu:[self setupMenu]];
+}
+
+- (NSMenu *)setupMenu
+{
+    NSMenu *menu = [[NSMenu alloc] init];
+    [menu addItem:[NSMenuItem separatorItem]];
+    [menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"];
+    
+    return menu;
 }
 
 @end
